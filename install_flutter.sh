0c4ece03a8ff8166e53f87c44bad44aeeded72e0
ravirikcapital-sudo <ravi.rikcapital@gmail.com>
2026-02-17 11:56:46 +0530

    Update install_flutter.sh

--- install_flutter.sh
+++ install_flutter.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
+
+set -e
+
+git clone https://github.com/flutter/flutter.git -b stable --depth 1
+export PATH="$PATH:`pwd`/flutter/bin"
+
+flutter doctor
+flutter config --enable-web
+flutter pub get
 
